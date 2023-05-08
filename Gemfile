bc7794151897e09d20486edda02802d5ba269d5c
mielonxd <kubamironczuk@onet.pl>
2023-05-08 19:38:24 +0200

    some views, controllers and bootstrap

--- Gemfile
+++ Gemfile
@@ -70,3 +70,8 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+#Bootstrap
+gem 'bootstrap', '~> 5.2', '>= 5.2.3'
+
+gem 'jquery-rails'
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  has_many :attachments, as: attachable,
+  has_many :attachments, as: :attachable
 end
